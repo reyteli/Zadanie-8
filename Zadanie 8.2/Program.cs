34d78813c78dd43074641cc70132e71f844be9b5
Никита Олегович Романенко <nikcom526@mail.ru>
2024-01-05 12:02:03 +0300

    Добавьте файлы проекта.

---
+++ Zadanie 8.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace Zadanie_8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Temp\Numbers.txt";
+            Random random = new Random();
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(-10, 10));
+                }
+            }
+            int k = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    k = k + Convert.ToInt32(sr.ReadLine());
+                     
+
+
+                }
+                Console.WriteLine(k);
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ Zadanie 8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+namespace Zadanie_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Temp";
+            string[] files = Directory.GetFiles(path, "*", SearchOption.AllDirectories);
+            foreach(string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+        }
+    }
+}
